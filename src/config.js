104a6da4bcbbc078ad07abf836c959ad5f42248b
xXUltraGunnerXx <Juan.Silvas@live.com>
2019-10-16 23:01:08 -0400

    adding files

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  API_TOKEN: process.env.API_TOKEN || 'dd35c785-451a-4168-99e3-073a03707a39',
+};
