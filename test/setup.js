f40608e4bbdd6d3699fb8efb665b51d985cd823e
xXUltraGunnerXx <Juan.Silvas@live.com>
2019-11-06 22:40:36 -0500

    adding files

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,8 @@
 process.env.NODE_ENV = 'test'
 process.env.API_TOKEN = 'test-auth-token'
 
+require('dotenv').config()
+
 const { expect } = require('chai')
 const supertest = require('supertest')
 
